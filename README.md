9ec86edd5dd90a5cf21cf3544782fb28ff461e48
Edward Hotchkiss <e@ingk.com>
2011-11-26 20:23:54 -0500

    0.0.1, new api format with better error checking

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
 
 # klout
 
-## NodeJS Klout API Wrapper 
+## NodeJS Klout API Wrapper (This is a rewrite of my old node-klout)
 
-### Download
+## Download
 
 ```bash
 $ npm install klout
 ```
 
-### Examples
+## Examples
+
+### Get a users Klout score
+
+```javascript
+var klout = require("klout");
+
+klout.configure("MY_API_KEY", "json", "1");
+
+klout.getKlout("kisshotch", function(error, users) {
+  console.log(users);
+});
+
+/* EOF */
+```
---
+++ test/index.test.js
@@ -0,0 +1,31 @@
+
+var vows = require("vows");
+var util = require("util");
+var assert = require("assert");
+var should = require("should");
+
+var klout = require("../lib/klout");
+
+klout.configure("YOUR_API_KEY", "json", "1");
+
+vows.describe("general module tests").addBatch({
+  "when requiring klout":{
+    topic:function(){ 
+      return klout;
+    },
+    "klout should be a function":function(topic){
+      topic.should.be.a("function");
+    }
+  },
+  "when requesting a single users klout":{
+    topic:function(){
+      klout.getKlout("kisshotch", this.callback);
+    },
+    "we should be able to get a klout score":function(error, users){
+      assert.equal(error, null);
+      users.should.be.a("object");
+    }
+  }
+}).export(module);
+
+/* EOF */
